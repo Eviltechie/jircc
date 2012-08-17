15440eb43f4dd366519d22f5ac8107b8409270db
Ivan Panarusky <eviltechie@joe.to>
2012-08-16 23:11:32 -0400

    initial commit, mvn

---
+++ src/main/java/to/joe/jircc/App.java
@@ -0,0 +1,13 @@
+package to.joe.jircc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
